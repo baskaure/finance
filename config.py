7e26da0f6f580339bbeb45efdb3f298d8c92bc83
tdorian <dorian.tatoulian@ynov.com>
2025-01-15 13:38:30 +0100

    a

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = 'votre_clé_secrète'  # À changer en production
+    SECRET_KEY = 'votre_clé_secrète' 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///expenses.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
